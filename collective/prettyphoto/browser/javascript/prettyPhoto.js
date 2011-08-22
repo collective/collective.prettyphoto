8bc700384c2b622ef802d21045e30147e1677a21
hschulz <h@schulz.com>
2011-08-22 08:52:23 +0000

    - working link to large image

--- collective/prettyphoto/browser/javascript/prettyPhoto.js
+++ collective/prettyphoto/browser/javascript/prettyPhoto.js
@@ -1,10 +1,17 @@
 jq(document).ready(function(){
     jq(document).ready(function(){
         // add rel tag for all links with class 'prettyPhoto'
+
         jq("a.prettyPhoto").attr({"rel": "prettyPhoto"});
 
+        jq("a.prettyPhoto img").each(function(index,value) {
+          var imgurl = jq(value).attr("src");
+          var lastunderscore = imgurl.lastIndexOf("_");
+          var imagebase = imgurl.slice(0,lastunderscore);
+          jq(value).parent("a.prettyPhoto").attr("href",imagebase + "_large");
+        });
         // add iframe attributes for all links with class 'prettyPhotoIframe'
-        jq("a.prettyPhotoIframe").attr("href", function() { 
+        jq("a.prettyPhotoIframe").attr("href", function() {
             return this.href + "?iframe=true&width=75%&height=75%";
         }).attr({"rel": "prettyPhoto"});
 
